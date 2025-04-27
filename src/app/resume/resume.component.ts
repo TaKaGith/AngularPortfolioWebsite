82cb4017d089104ee9466ac6807160e1f6534b83
akifaziz98 <akifaziz98@gmail.com>
2025-04-27 15:49:23 +0800

    Update Resume Page / Accodion Heading Placeholder

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -10,6 +10,9 @@ import { Title } from '@angular/platform-browser';
 export class ResumeComponent {
   
 isWorkExperienceOpen = false;
+isEducationOpen = false;
+isCertificationOpen = false;
+isSkillOpen = false;
 
   constructor(private titleService: Title, private render: Renderer2) {
     this.titleService.setTitle('Akif Aziz | Resume');
