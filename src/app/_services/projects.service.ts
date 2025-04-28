10a09f7b6cea4b41b78b8ace3dcf8aaf50048714
akifaziz98 <akifaziz98@gmail.com>
2025-04-28 22:30:09 +0800

    Add software engineer project 1

--- src/app/_services/projects.service.ts
+++ src/app/_services/projects.service.ts
@@ -12,7 +12,7 @@ export class ProjectsService {
       name: 'Sample Angular Project',
       summary: 'Test Description',
       description: 'This is sample Angular Project',
-      projectLink: 'https://github.com/TaKaGith',
+      projectLink: '',
       pictures: ["assets/image_1.jpg","assets/image_2.jpg","assets/image_3.jpg"],
       tags: [Tag.ANGULAR, Tag.TYPESCRIPT],
     },
